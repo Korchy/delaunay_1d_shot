045b7cf78b6d32f08455249866774990562498f1
Nikita Akimov <korchiy@yandex.ru>
2020-06-07 10:46:25 +0300

    update

---
+++ __init__.py
@@ -0,0 +1,24 @@
+
+from . import delaunayVoronoiBlender
+
+bl_info = {
+	"name": "Delaunay Voronoi ",
+	"description": "Points cloud Delaunay triangulation in 2.5D (suitable for terrain modelling) or Voronoi diagram in 2D",
+	"author": "Domlysz",
+	'license': 'GPL',
+	'deps': '',
+	"version": (1, 3),
+	"blender": (2, 7, 0),
+	"location": "View3D > Tools > GIS",
+	"warning": "",
+	'wiki_url': 'https://github.com/domlysz/BlenderGIS/wiki',
+	'tracker_url': '',
+	'link': '',
+	"category": "Mesh"}
+
+
+def register():
+	delaunayVoronoiBlender.register()
+
+def unregister():
+	delaunayVoronoiBlender.unregister()
